04cadbdde94d6c465b444d0a592a091f386be16d
Navin P <navinp0304@gmail.com>
2022-05-07 16:45:23 +0530

    AddStock.java clean

--- gkt-portfolio-overlap/src/main/java/com/example/geektrust/AddStock.java
+++ gkt-portfolio-overlap/src/main/java/com/example/geektrust/AddStock.java
@@ -5,7 +5,7 @@ import java.util.Map;
 import java.util.Set;
 
 public class AddStock implements IStockCommand {
-	private Map<String, Set<String>> completePortFolio;
+	private final Map<String, Set<String>> completePortFolio;
 	private final List<String> currentPortFolio;
 
 	AddStock(Map<String, Set<String>> completePortFolio, List<String> currentPortFolio) {
@@ -16,11 +16,14 @@ public class AddStock implements IStockCommand {
 	@Override
 	public List<String> execute(String fullCommand) {
 		String[] commands = fullCommand.split(" ");
-		String fundName = commands[1];
-		String stockName = fullCommand.substring(fullCommand.indexOf(fundName) + fundName.length()).trim();
-		try {
-			completePortFolio.get(fundName).add(stockName);
-		} catch (Exception ex) {
+		String fundName = new String(commands[1]);
+		int stockNameOffset = fullCommand.indexOf(fundName) + fundName.length();
+		String stockNameFull = new String(fullCommand.substring(stockNameOffset));
+		String stockName=stockNameFull.trim();
+		Set<String> collectionSet = completePortFolio.get(fundName);
+		if (collectionSet != null) {
+			collectionSet.add(stockName);
+		} else {
 			System.out.println("FUND_NOT_FOUND");
 		}
 		return this.currentPortFolio;
